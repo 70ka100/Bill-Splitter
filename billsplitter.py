8780decd342ee5acd02d6ec532a584599b58b4f6
Mahmoud Mostafa <52200270+70ka100@users.noreply.github.com>
2024-12-24 11:46:13 +0200

    Update billsplitter.py

--- billsplitter.py
+++ billsplitter.py
@@ -48,3 +48,55 @@ else:
     else:
         not_lucky(bill_, num_of_friends_)
     print(result)
+
+
+# Enter the number of friends joining (including you):
+# > 5
+# 
+# Enter the name of every friend (including you), each on a new line:
+# > Marc
+# > Jem
+# > Monica
+# > Anna
+# > Jason
+#
+# Enter the total bill value:
+# > 100
+# 
+# Do you want to use the "Who is lucky?" feature? Write Yes/No:
+# > Yes
+# 
+# Jem is the lucky one!
+# 
+# {'Marc': 25, 'Jem': 0, 'Monica': 25, 'Anna': 25, 'Jason': 25}
+
+
+
+
+# Enter the number of friends joining (including you):
+# > 5
+# 
+# Enter the name of every friend (including you), each on a new line:
+# > Marc
+# > Jem
+# > Monica
+# > Anna
+# > Jason
+#
+# Enter the total bill value:
+# > 100
+# 
+# Do you want to use the "Who is lucky?" feature? Write Yes/No:
+# > No
+#
+# No one is going to be lucky
+#
+# {'Marc': 20, 'Jem': 20, 'Monica': 20, 'Anna': 20, 'Jason': 20}
+
+
+
+
+# Enter the number of friends joining (including you):
+# > 0
+# 
+# No one is joining for the party
